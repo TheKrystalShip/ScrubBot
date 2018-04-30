9c09dc175e34ef267250e6fd2cb4f14a50d48458
Sjouke Yntema <suyntema@live.nl>
2018-04-30 21:11:20 +0200

    Initial commit

---
+++ ScrubBot/Core/Program.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+using System.Threading.Tasks;
+
+namespace ScrubBot
+{
+    public class Program
+    {
+        private string _token;
+        private DiscordSocketClient _client;
+
+        //private EventHandler _eventHandler;
+
+        static void Main(string[] args) => new Program().Initialize().Wait();
+
+        private async Task Initialize()
+        {
+            _token = Properties.Resources.LoginToken;
+            _client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                DefaultRetryMode = RetryMode.AlwaysRetry,
+                ConnectionTimeout = 5000,
+                LogLevel = LogSeverity.Debug,
+                AlwaysDownloadUsers = true
+            });
+
+            //_eventHandler = new EventHandler();
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await _client.SetGameAsync(Properties.Resources.ActiveGame);
+
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ ScrubBot/Handlers/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ScrubBot.Handlers
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commandService;
+        private IServiceProvider _serviceCollection;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _commandService = new CommandService(new CommandServiceConfig()
+            {
+                CaseSensitiveCommands = false,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            //_commandService.AddModuleAsync(Assembly.GetEntryAssembly());
+        }
+    }
+}
