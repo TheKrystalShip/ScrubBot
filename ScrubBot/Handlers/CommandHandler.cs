542b7e7b29def756f631f6b61f4ddfde0f2412f0
Sjouke Yntema <suyntema@live.nl>
2018-05-01 15:30:12 +0200

    Complete basic bot setup

---
+++ ScrubBot/Handlers/CommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScrubBot.Handlers
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commandService;
+        private IServiceProvider _serviceCollection;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _commandService = new CommandService(new CommandServiceConfig()
+            {
+                CaseSensitiveCommands = false,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            _commandService.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _serviceCollection = new ServiceCollection().BuildServiceProvider();
+
+            Console.Title = @"ScrubBot";
+            _client.MessageReceived += HandleCommand;
+        }
+
+        private async Task HandleCommand(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message == null || message.Author.IsBot) return;
+
+            int argPos = 0;
+            bool mention = message.HasMentionPrefix(_client.CurrentUser, ref argPos);
+
+            if (!mention) return;
+
+            SocketCommandContext context = new SocketCommandContext(_client, message);
+            IResult result = await _commandService.ExecuteAsync(context, argPos, _serviceCollection);
+
+            if (result.IsSuccess) return;
+            Console.WriteLine(new LogMessage(LogSeverity.Error, "Command", result.ErrorReason));
+        }
+    }
+}
---
+++ ScrubBot/Handlers/EventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace ScrubBot.Handlers
+{
+    public class EventHandler
+    {
+        private DiscordSocketClient _client;
+
+        public EventHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.Log += LogMessage;
+        }
+
+        private async Task LogMessage(LogMessage message)
+        {
+            if (message.Message.Contains("OpCode")) return;
+
+            await Task.Run(() => { Console.WriteLine(message); });
+        }
+    }
+}
