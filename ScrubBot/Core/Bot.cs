08de5f8081413c7aa52f9623671e4e511c837a93
Cristian Moraru <heisenberg@thekrystalship.org>
2018-10-11 21:27:08 +0200

    Separated bot logic into dedicated class.

---
+++ ScrubBot/Core/Bot.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+using ScrubBot.Handlers;
+using ScrubBot.Properties;
+
+using System.Threading.Tasks;
+
+namespace ScrubBot
+{
+    public class Bot
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandHandler _commandHandler;
+        private readonly string _token;
+
+        public Bot()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug,
+                DefaultRetryMode = RetryMode.AlwaysRetry,
+                ConnectionTimeout = 5000,
+                AlwaysDownloadUsers = true
+            });
+
+            _commandHandler = new CommandHandler(ref _client);
+
+            _token = Resources.LoginToken;
+        }
+
+        public async Task InitAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await _client.SetGameAsync("Type >Help for help");
+        }
+    }
+}
--- ScrubBot/Core/Program.cs
+++ ScrubBot/Core/Program.cs
@@ -1,8 +1,4 @@
-﻿using Discord;
-using Discord.WebSocket;
-
-using ScrubBot.Handlers;
-using ScrubBot.Properties;
+﻿using ScrubBot.Extensions;
 
 using System.Threading.Tasks;
 
@@ -10,29 +6,13 @@ namespace ScrubBot
 {
     public class Program
     {
-        // Only one instance per app lifetime
-        private static DiscordSocketClient _client;
-        private static CommandHandler _commandHandler;
-        private static readonly string _token = Resources.LoginToken;
+        private static Bot _scrubBot;
 
         public static async Task Main(string[] args)
         {
-            _client = new DiscordSocketClient(new DiscordSocketConfig
-            {
-                LogLevel = LogSeverity.Debug,
-                DefaultRetryMode = RetryMode.AlwaysRetry,
-                ConnectionTimeout = 5000,
-                AlwaysDownloadUsers = true
-            });
-
-            // Just reference the _client, no need to copy it
-            _commandHandler = new CommandHandler(ref _client);
-
-            await _client.LoginAsync(TokenType.Bot, _token);
-            await _client.StartAsync();
-            await _client.SetGameAsync("Type >Help for help");
-
-            await Task.Delay(-1);
+            await (_scrubBot = new Bot())
+                .InitAsync()
+                .DelayIndefinetly();
         }
     }
 }
