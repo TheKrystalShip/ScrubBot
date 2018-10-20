cfeef4a9be2caff3439c53781d18f2863bb6478a
Cristian Moraru <heisenberg@thekrystalship.org>
2018-10-20 12:12:30 +0200

    Updated Settings.cs to ensure settings & secrets files always get created if non-existent upon launch.

--- ScrubBot/Properties/Settings.cs
+++ ScrubBot/Properties/Settings.cs
@@ -6,20 +6,26 @@ namespace ScrubBot.Properties
 {
     public class Settings
     {
-        static Settings()
-        {
-            string settingsFile = Path.Combine("Properties", "settings.json");
-            if (!File.Exists(settingsFile ))
-                File.Create(settingsFile );
-        }
+        private readonly static string _settingsFile = Path.Combine("Properties", "settings.json");
+        private readonly static string _secretsFile = Path.Combine("Properties", "secrets.json");
 
         private static IConfiguration _config;
 
         public static IConfiguration Instance => _config ??
             (_config = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Properties"))
-                .AddJsonFile("settings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile("secrets.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(path: _settingsFile, optional: false, reloadOnChange: true)
+                .AddJsonFile(path: _secretsFile, optional: false, reloadOnChange: true)
                 .Build());
+
+        static Settings()
+        {
+            Directory.CreateDirectory("Properties");
+
+            if (!File.Exists(_settingsFile))
+                File.Create(_settingsFile);
+
+            if (!File.Exists(_secretsFile))
+                File.Create(_secretsFile);
+        }
     }
 }
