fe27f4b7d963ab065f2130fe81084ea209551674
Sjouke Yntema <suyntema@live.nl>
2018-11-21 11:36:59 +0100

    Implement event logic

--- ScrubBot/Handlers/ServiceHandler.cs
+++ ScrubBot/Handlers/ServiceHandler.cs
@@ -1,9 +1,12 @@
-﻿using Discord.WebSocket;
+﻿using System.Linq;
+using Discord.WebSocket;
 using ScrubBot.Core;
 using ScrubBot.Database;
 using ScrubBot.Domain;
 
 using System.Threading.Tasks;
+using Discord;
+using ScrubBot.Extensions;
 
 namespace ScrubBot.Handlers
 {
@@ -20,14 +23,18 @@ namespace ScrubBot.Handlers
 
         public async Task OnBirthdayServiceTriggerAsync(User user)
         {
-            // Logic
-
+            await _client.GetUser(user.Id).SendMessageAsync(string.Empty, false, new EmbedBuilder().CreateMessage("Hey kanjer, dit is je verjaardag!",
+                                                                                                                  $"Van harte gefeliciflapstaart met je verjaardag **{user.Username}**! Hiep hiep hoera en een fijne dag!"));
             await Task.CompletedTask;
         }
 
         public async Task OnEventServiceTriggerAsync(Event @event)
         {
-            // Logic
+            foreach (var subscriber in @event.Subscribers)
+                await _client.GetUser(subscriber.Id).SendMessageAsync(string.Empty, false, new EmbedBuilder().CreateMessage("Event reminder", $"Dear {subscriber.Username},\t" +
+                                                                                                                                              $"**{@event.Author.Username}{(@event.Author.Username.Last() == 's' ? "'" : "'s")}** event **{@event.Title}** is about to start!"));
+
+            await _client.GetUser(@event.Author.Id).SendMessageAsync(string.Empty, false, new EmbedBuilder().CreateMessage("Event reminder", $"Your event **{@event.Title}** is about to start!"));
 
             await Task.CompletedTask;
         }
