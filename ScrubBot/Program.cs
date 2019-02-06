e108cc5977902d5d4666d60f639c386ab63f7c02
DevCas1 <suyntema@live.nl>
2019-02-06 17:08:53 +0100

    Add Console Title

--- ScrubBot/Program.cs
+++ ScrubBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using ScrubBot.Core;
 
@@ -8,6 +9,7 @@ namespace ScrubBot
     {
         public static async Task Main(string[] args)
         {
+            Console.Title = "ScrubBot";
             await BotBuilder
                 .UseStartup<Startup>()
                 .ConfigureContainer()
