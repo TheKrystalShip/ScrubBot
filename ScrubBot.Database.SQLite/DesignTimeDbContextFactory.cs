8612b21b3c2ddd1f02d6205d38c2fad0f64f4fae
Cristian Moraru <cristian.moraru@live.com>
2019-05-01 23:32:14 +0200

    Fixed DesignTimeDbContextFactory not being able to read settings.json because it was never loaded into memory.

--- ScrubBot.Database.SQLite/DesignTimeDbContextFactory.cs
+++ ScrubBot.Database.SQLite/DesignTimeDbContextFactory.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+﻿using System.IO;
 
-using ScrubBot.Database.SQLite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 using TheKrystalShip.Tools.Configuration;
 
-namespace ScrubBot.Database.Core
+namespace ScrubBot.Database.SQLite
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SQLiteContext>
     {
         public SQLiteContext CreateDbContext(string[] args)
         {
             DbContextOptionsBuilder<SQLiteContext> builder = new DbContextOptionsBuilder<SQLiteContext>();
+
+            Configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Properties"));
+#if DEBUG
+            Configuration.AddFiles("settings.dev.json");
+#else
+            Configuration.AddFiles("settings.json");
+#endif
 
             builder.UseSqlite(Configuration.GetConnectionString("SQLite"));
 
