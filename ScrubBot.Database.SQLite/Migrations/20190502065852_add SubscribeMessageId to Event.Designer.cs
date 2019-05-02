9eb59f3da0bc1f357cb77c08f56b0ca6518292f4
DevCas1 <suyntema@live.nl>
2019-05-02 08:59:15 +0200

    Add SubscribeMessageId to Event

--- ScrubBot.Database.SQLite/Migrations/20190502065852_add SubscribeMessageId to Event.Designer.cs
+++ ScrubBot.Database.SQLite/Migrations/20190502065852_add SubscribeMessageId to Event.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScrubBot.Database.SQLite;
 
 namespace ScrubBot.Database.SQLite.Migrations
 {
     [DbContext(typeof(SQLiteContext))]
-    partial class SQLiteContextModelSnapshot : ModelSnapshot
+    [Migration("20190502065852_add SubscribeMessageId to Event")]
+    partial class addSubscribeMessageIdtoEvent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ScrubBot.Database.SQLite/Migrations/20190502065852_add SubscribeMessageId to Event.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScrubBot.Database.SQLite.Migrations
+{
+    public partial class addSubscribeMessageIdtoEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubscribeMessageId",
+                table: "Events",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscribeMessageId",
+                table: "Events");
+        }
+    }
+}
