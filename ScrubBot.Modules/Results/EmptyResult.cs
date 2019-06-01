6e6a0772c5d8c51cd4d50b0f769190f1dd7a5340
DevCas1 <suyntema@live.nl>
2019-06-01 20:44:59 +0200

    Update EmptyResult class with more constructors

--- ScrubBot.Modules/Results/EmptyResult.cs
+++ ScrubBot.Modules/Results/EmptyResult.cs
@@ -1,10 +1,18 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 
 namespace ScrubBot.Modules
 {
     public class EmptyResult : RuntimeResult
     {
+        public Embed Embed { get; }
+
         public EmptyResult(CommandError? error, string reason) : base(error, reason)
+        {
+
+        }
+
+        public EmptyResult(string message) : this(null, message)
         {
 
         }
@@ -12,6 +20,16 @@ namespace ScrubBot.Modules
         public EmptyResult() : this(null, null)
         {
 
+        }
+
+        public EmptyResult(EmbedBuilder embedBuilder) : this(null, "Success")
+        {
+            Embed = embedBuilder.Build();
+        }
+
+        public EmptyResult(Embed embed) : this(null, "Success")
+        {
+            Embed = embed;
         }
     }
 }
